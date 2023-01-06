9bf7619ca7c1cb54b60c3bb604208385211c40e2
obsesiva-syntaxis <obsesivasyntaxis@gmail.com>
2023-01-06 16:35:54 -0300

    first commit for custom-pokeapi

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,45 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose/dist';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (err) {
+      if( err.code === 11000){
+        throw new BadRequestException(`Pokemon exists in database ${ JSON.stringify( err.keyValue )}`)
+      }
+      console.log(err);
+      throw new InternalServerErrorException(`Can+t create Pokemon - Check server logs`);
+    }
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
